ddd418cca1317e57650d173b2578d0e32ea352d1
lmonsalve22 <lmonsalve22@gmail.com>
2025-10-30 15:55:46 -0300

    display

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/REACT_SIMPLE/' // <-- cambia esto al nombre de tu repo en GitHub
+  base: '/REACT_SIMPLE/', // 👈 importante para GitHub Pages
 })
+
